b649d8d3c161a045ab0c59faf5894229c095e845
Aurelian CD <caurelian85@gmail.com>
2015-03-31 13:57:09 +0300

    Create Project
    Create Project - for test

---
+++ ThumbnailImage/ThumbnailImage/IOUtils.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+
+namespace ThumbnailImage
+{
+    public class IOUtils
+    {
+        public static void WriteBinaryFile(string fileName, byte[] contents)
+        {
+            using (FileStream fs = OpenFileStreamForWriting(fileName))
+            {
+                using (BinaryWriter bw = new BinaryWriter(fs))
+                {
+                    bw.Write(contents);
+                    bw.Close();
+                }
+            }
+        }
+
+        public static FileStream OpenFileStreamForWriting(string path)
+        {
+            return new FileStream(path, FileMode.Create, FileAccess.Write);
+        }
+    }
+}
---
+++ ThumbnailImage/ThumbnailImage/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ThumbnailImage
+{
+    class Program
+    {
+        private const string ImageSourcePath="Do not forgot to set the path!";
+        private const string ImageResultPath = "Do not forgot to set the path!";
+
+        private static void Main(string[] args)
+        {
+            var thumb  = ImageUtils.CreateThumbnailImage(ImageSourcePath, System.Drawing.Imaging.ImageFormat.Png, 156, 156);
+            IOUtils.WriteBinaryFile(ImageResultPath, thumb);
+            Console.ReadKey();
+        }
+    }
+}
